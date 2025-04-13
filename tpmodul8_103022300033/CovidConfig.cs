560233b3a197c81a530c85c923c6dca27bf7d030
ilhamridzuan <ilhamridzuaan@gmail.com>
2025-04-14 02:43:42 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_103022300033/CovidConfig.cs
@@ -0,0 +1,12 @@
+﻿public class CovidConfig
+{
+    public string satuan_suhu { get; set; } = "celcius";
+    public int batas_hari_deman { get; set; } = 14;
+    public string pesan_ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+    public string pesan_diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+    public void UbahSatuan()
+    {
+        satuan_suhu = satuan_suhu == "celcius" ? "fahrenheit" : "celcius";
+    }
+}
--- tpmodul8_103022300033/Program.cs
+++ tpmodul8_103022300033/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+        CovidConfig config;
+        if (File.Exists("covid_config.json"))
+        {
+            string json = File.ReadAllText("covid_config.json");
+            config = JsonConvert.DeserializeObject<CovidConfig>(json);
+        }
+        else
+        {
+            config = new CovidConfig();
+        }
+
+        Console.Write($"Berapa suhu badan anda saat ini? (dalam {config.satuan_suhu}): ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam? ");
+        int hari = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuOK = false;
+
+        if (config.satuan_suhu == "celcius")
+            suhuOK = suhu >= 36.5 && suhu <= 37.5;
+        else
+            suhuOK = suhu >= 97.7 && suhu <= 99.5;
+
+        if (suhuOK && hari < config.batas_hari_deman)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+
+        config.UbahSatuan();
+
+        string updatedConfig = JsonConvert.SerializeObject(config, Formatting.Indented);
+        File.WriteAllText("covid_config.json", updatedConfig);
+    }
+}
